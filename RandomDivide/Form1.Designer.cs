64be53708482d359e341daeb664e2693e636288d
ISDHN <53806614+ISDHN@users.noreply.github.com>
2020-10-25 11:15:36 +0800

    Initial commit

---
+++ RandomDivide/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace RandomDivide
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.GroupList = new System.Windows.Forms.ListView();
+            this.Class = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Group = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Create = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.ClassIndex = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Senior1 = new System.Windows.Forms.Button();
+            this.Senior2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // GroupList
+            // 
+            this.GroupList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Class,
+            this.Group,
+            this.ClassIndex});
+            this.GroupList.HideSelection = false;
+            this.GroupList.LabelEdit = true;
+            this.GroupList.Location = new System.Drawing.Point(24, 12);
+            this.GroupList.Name = "GroupList";
+            this.GroupList.ShowItemToolTips = true;
+            this.GroupList.Size = new System.Drawing.Size(192, 243);
+            this.GroupList.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.GroupList, "按\"+\"按钮 添加班级,按\"Delete键\"删除班级。");
+            this.GroupList.UseCompatibleStateImageBehavior = false;
+            this.GroupList.View = System.Windows.Forms.View.Details;
+            this.GroupList.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GroupList_KeyDown);
+            // 
+            // Class
+            // 
+            this.Class.Text = "班级";
+            this.Class.Width = 50;
+            // 
+            // Group
+            // 
+            this.Group.Text = "组别";
+            // 
+            // Create
+            // 
+            this.Create.Location = new System.Drawing.Point(24, 261);
+            this.Create.Name = "Create";
+            this.Create.Size = new System.Drawing.Size(192, 23);
+            this.Create.TabIndex = 3;
+            this.Create.Text = "生成";
+            this.Create.UseVisualStyleBackColor = true;
+            this.Create.Click += new System.EventHandler(this.Create_Click);
+            // 
+            // ClassIndex
+            // 
+            this.ClassIndex.Text = "班级编号";
+            // 
+            // Senior1
+            // 
+            this.Senior1.Location = new System.Drawing.Point(222, 123);
+            this.Senior1.Name = "Senior1";
+            this.Senior1.Size = new System.Drawing.Size(65, 23);
+            this.Senior1.TabIndex = 5;
+            this.Senior1.Text = "高一";
+            this.Senior1.UseVisualStyleBackColor = true;
+            this.Senior1.Click += new System.EventHandler(this.Senior1_Click);
+            // 
+            // Senior2
+            // 
+            this.Senior2.Location = new System.Drawing.Point(222, 170);
+            this.Senior2.Name = "Senior2";
+            this.Senior2.Size = new System.Drawing.Size(65, 23);
+            this.Senior2.TabIndex = 6;
+            this.Senior2.Text = "高二";
+            this.Senior2.UseVisualStyleBackColor = true;
+            this.Senior2.Click += new System.EventHandler(this.Senior2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 296);
+            this.Controls.Add(this.Senior2);
+            this.Controls.Add(this.Senior1);
+            this.Controls.Add(this.Create);
+            this.Controls.Add(this.GroupList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "班级分组";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListView GroupList;
+        private System.Windows.Forms.ColumnHeader Class;
+        private System.Windows.Forms.ColumnHeader Group;
+        private System.Windows.Forms.Button Create;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ColumnHeader ClassIndex;
+        private System.Windows.Forms.Button Senior1;
+        private System.Windows.Forms.Button Senior2;
+    }
+}
+
---
+++ RandomDivide/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RandomDivide
+{
+    public partial class Form1 : Form
+    {
+        private int ClassNumber=1;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:命名样式", Justification = "<挂起>")]
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GroupList.Items.Add("班级" + ClassNumber);
+            
+            ClassNumber += 1;
+        }
+
+        private void GroupList_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.Delete && GroupList.SelectedItems.Count!=0)
+            {
+                GroupList.Items.Remove(GroupList.SelectedItems[0]);
+            }
+        }
+
+        private void Create_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem g in GroupList.Items)
+            {
+                if (g.SubItems.Count >1) g.SubItems.RemoveAt(1);
+            }
+            Random random = new Random();
+            int count = GroupList.Items.Count;
+            List<string> gname = new List<string>();
+            for(int i = 0; i < count/4; i++)
+            {
+                for(int j = 0;j<4; j++)
+                    gname.Add(((char)(i+65)).ToString());
+            }
+            foreach (ListViewItem g in GroupList.Items)
+            {
+                int name = random.Next(gname.Count);
+                int availablecount = gname.FindAll((string gn) => {
+                    return gn == gname[name].ToString();
+                }).Count;
+                
+                g.SubItems.Add(gname[name].ToString());
+                g.SubItems.Add((4-availablecount+1).ToString());
+                gname.RemoveAt(name);              
+            }
+        }
+
+        private void Senior1_Click(object sender, EventArgs e)
+        {
+            ClassNumber = 1;
+            GroupList.Items.Clear();
+            for (int i = 0; i < 20; i++)
+            {
+                GroupList.Items.Add(ClassNumber + "班");
+
+                ClassNumber += 1;
+            }
+
+        }
+
+        private void Senior2_Click(object sender, EventArgs e)
+        {
+            ClassNumber = 1;
+            GroupList.Items.Clear();
+            for (int i = 0; i < 16; i++)
+            {
+                GroupList.Items.Add(ClassNumber + "班");
+
+                ClassNumber += 1;
+            }
+        }
+    }
+}
